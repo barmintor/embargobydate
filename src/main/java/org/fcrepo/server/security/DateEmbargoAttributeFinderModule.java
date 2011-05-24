7a5d703213fe539d1b3e362d016c4ee5bbdc0975
Ben Armintor <armintor@gmail.com>
2011-05-24 17:33:54 -0400

    rels, rels-int, and props; no test

--- src/main/java/org/fcrepo/server/security/DateEmbargoAttributeFinderModule.java
+++ src/main/java/org/fcrepo/server/security/DateEmbargoAttributeFinderModule.java
@@ -9,9 +9,11 @@ import java.util.Date;
 import java.util.Set;
 
 import org.jrdf.graph.PredicateNode;
+import org.jrdf.graph.SubjectNode;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.BeanInitializationException;
 
 import org.fcrepo.common.Constants;
 import org.fcrepo.common.rdf.SimpleURIReference;
@@ -19,11 +21,14 @@ import org.fcrepo.common.rdf.SimpleURIReference;
 import org.fcrepo.server.ReadOnlyContext;
 import org.fcrepo.server.Server;
 import org.fcrepo.server.errors.ServerException;
+import org.fcrepo.server.resourceIndex.ResourceIndex;
 import org.fcrepo.server.storage.DOManager;
 import org.fcrepo.server.storage.DOReader;
 import org.fcrepo.server.storage.types.RelationshipTuple;
 
 import com.sun.xacml.EvaluationCtx;
+import com.sun.xacml.attr.StringAttribute;
+import com.sun.xacml.cond.EvaluationResult;
 
 public class DateEmbargoAttributeFinderModule
 extends AttributeFinderModule {
@@ -34,36 +39,62 @@ extends AttributeFinderModule {
 	public static final String ATTRIBUTE_URI = Constants.RESOURCE.uri + ":embargoed";
     public static final String ATTRIBUTE_TYPE = null; // String
 	
-    private final EmbargoDatePropertyType m_propType;
+    private EmbargoDatePropertyType m_propType;
+    private String m_dsId;
+    private ResourceIndex m_rindex;
     private final String m_propName;
     private final String m_dateFormat;
-    private final PredicateNode m_propNode;
-	public DateEmbargoAttributeFinderModule(
-			EmbargoDatePropertyType propType,
-			String propName,
-			String dateFormat){
-		super();
-		m_propType = propType;
-		m_propName = propName;
-		m_dateFormat = dateFormat;
-		PredicateNode propNode = null;
-		try {
-			if (m_propType == EmbargoDatePropertyType.RELSEXT) {
-				propNode = new SimpleURIReference(new URI(m_propName));
-			}
-			else {
-				propNode = null;
-			}
-			registerAttribute(ATTRIBUTE_URI,ATTRIBUTE_TYPE);
+    private final PredicateNode m_pnode;
+    public static DateEmbargoAttributeFinderModule getFromRELSEXT(String propName, String dateFormat) {
+        return new DateEmbargoAttributeFinderModule(propName, dateFormat);
+    }
+    private DateEmbargoAttributeFinderModule(
+            String propName,
+            String dateFormat) {
+        m_propType = EmbargoDatePropertyType.OBJECT_PROPS;
+        m_propName = propName;
+        m_dateFormat = dateFormat;
+        PredicateNode propNode = null;
+        try {
+            if (m_propType == EmbargoDatePropertyType.OBJECT_RELS) {
+                propNode = new SimpleURIReference(new URI(m_propName));
+            }
+            else {
+                propNode = null;
+            }
+            registerAttribute(ATTRIBUTE_URI,ATTRIBUTE_TYPE);
             setInstantiatedOk(true);
-		}
-		catch (URISyntaxException e){
+        }
+        catch (URISyntaxException e){
             setInstantiatedOk(false);
-		}
-		finally {
-			m_propNode = propNode;
-		}
-	}
+        }
+        finally {
+            m_pnode = propNode;
+        }
+    }
+    public void setType(EmbargoDatePropertyType type) {
+        m_propType = type;
+    }
+
+    public void setDatastreamId(String dsId) {
+        m_dsId = dsId;
+    }
+    
+    public void setResourceIndex(ResourceIndex rindex) {
+        m_rindex = rindex;
+    }
+    
+    public void init() throws BeanInitializationException {
+        switch (m_propType){
+            case OBJECT_PROPS:
+                break;
+            case OBJECT_RELS:
+                break;
+            case RINDEX:
+                if (m_rindex == null) throw new BeanInitializationException("resourceIndex property not set");
+        }
+    }
+    
 	@Override
 	protected boolean canHandleAdhoc() {
 		return false;
@@ -91,25 +122,53 @@ extends AttributeFinderModule {
 							ReadOnlyContext.EMPTY,
 							pid);
 			} catch (ServerException e) {
-				logger.debug("couldn't get object reader");
+				logger.error("couldn't get object reader",e);
 				return null;
 			}
 			String[] values = null;
+
             switch (m_propType){
-            case OBJECT:
-            	break;
-            case RELSEXT:
-            	break;
+            case OBJECT_PROPS:
+                try{
+                    long end = getLatestDateFromObject(reader);
+                    values = new String[]{Boolean.toString((getAttributeStartTime >= end))};
+                } catch (ServerException e) {
+                    logger.error("couldn't get object relationships",e);
+                }
+                break;
+            case OBJECT_RELS:
+                SubjectNode snode = null;
+                if (m_dsId == null) { // search RELS-EXT for object triple
+                    snode = null;
+                } else { // search RELS-INT for datastream triple
+                    if (!m_dsId.equals(getDatastreamId(context))){
+                        return null;
+                    }
+                    try{
+                        String snode_uri = "info:fedora/" + reader.GetObjectPID() + "/" + m_dsId;
+                        snode = new SimpleURIReference(new URI(snode_uri));
+                    } catch (Throwable t) {
+                        logger.error("couldn't get object pid or uri", t);
+                    }
+                }
+                try{
+            	    long end = getLatestDateFromRels(reader, snode);
+            	    values = new String[]{Boolean.toString((getAttributeStartTime >= end))};
+                } catch (ServerException e) {
+                    logger.error("couldn't get object relationships",e);
+                }
+                break;
             default:
             	logger.warn("Unknown property type for date embargo: " + m_propType);
-            	return null;
             }
 
 			return values;
 		} finally {
-			long dur = System.currentTimeMillis() - getAttributeStartTime;
-			logger.debug("Locally getting the '" + attributeId
-					+ "' attribute for this resource took " + dur + "ms.");
+			if (logger.isDebugEnabled()){
+	            long dur = System.currentTimeMillis() - getAttributeStartTime;
+	            logger.debug("Locally getting the '" + attributeId
+	                         + "' attribute for this resource took " + dur + "ms.");
+			}
 		}
 	}
 
@@ -121,9 +180,9 @@ extends AttributeFinderModule {
 		}
 	}
 	
-	private long getDateFromRels(DOReader reader) throws ServerException {
+	private long getLatestDateFromRels(DOReader reader, SubjectNode snode) throws ServerException {
 		long result = -1;
-		Set<RelationshipTuple> rels = reader.getRelationships(m_propNode, null);
+		Set<RelationshipTuple> rels = reader.getRelationships(snode, m_pnode, null);
 		if (!rels.isEmpty()){
 			DateFormat df = new SimpleDateFormat(m_dateFormat);
 			for (RelationshipTuple rel:rels){
@@ -137,6 +196,72 @@ extends AttributeFinderModule {
 		}
 		return result;
 	}
+	
+    private long getLatestDateFromObject(DOReader reader) throws ServerException {
+        long result = -1;
+        String prop = reader.getObject().getExtProperty(m_propName);
+        if (prop != null){
+            DateFormat df = new SimpleDateFormat(m_dateFormat);
+            try {
+                result = df.parse(prop).getTime();
+            } catch (ParseException e) {
+                logger.warn("Could not parse date property: " + prop);
+            }
+        }
+        return result;
+    }	
+    private final String getDatastreamId(EvaluationCtx context) {
+        URI datastreamIdUri = null;
+        try {
+            datastreamIdUri = new URI(Constants.DATASTREAM.ID.uri);
+        } catch (URISyntaxException e) {
+        }
+
+        EvaluationResult attribute =
+                context.getResourceAttribute(STRING_ATTRIBUTE_URI,
+                                             datastreamIdUri,
+                                             null);
+
+        Object element = getAttributeFromEvaluationResult(attribute);
+        if (element == null) {
+            logger.debug("getDatastreamId: " + " exit on "
+                    + "can't get resource-id on request callback");
+            return null;
+        }
+
+        if (!(element instanceof StringAttribute)) {
+            logger.debug("getDatastreamId: " + " exit on "
+                    + "couldn't get resource-id from xacml request "
+                    + "non-string returned");
+            return null;
+        }
+
+        String datastreamId = ((StringAttribute) element).getValue();
+
+        if (datastreamId == null) {
+            logger.debug("getDatastreamId: " + " exit on " + "null resource-id");
+            return null;
+        }
+
+        if (!validDatastreamId(datastreamId)) {
+            logger.debug("invalid resource-id: datastreamId is not valid");
+            return null;
+        }
+
+        return datastreamId;
+    }
+
+    private final boolean validDatastreamId(String datastreamId) {
+        if (datastreamId == null) {
+            return false;
+        }
+        // "" is a valid resource id, for it represents a don't-care condition
+        if (" ".equals(datastreamId)) {
+            return false;
+        }
+        return true;
+    }
+
 
 }
 
--- src/main/java/org/fcrepo/server/security/EmbargoDatePropertyType.java
+++ src/main/java/org/fcrepo/server/security/EmbargoDatePropertyType.java
@@ -1,5 +1,5 @@
 package org.fcrepo.server.security;
 
 public enum EmbargoDatePropertyType {
-    OBJECT, RELSEXT
+    OBJECT_PROPS, OBJECT_RELS, RINDEX
 }
